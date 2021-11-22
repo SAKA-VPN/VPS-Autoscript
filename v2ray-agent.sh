a330695484d9fda63acbd019a0408479af877d6f
SAKA VPN <93951947+SAKA-VPN@users.noreply.github.com>
2021-11-23 04:36:00 +0800

    Update v2ray-agent.sh
    added script

--- v2ray-agent.sh
+++ v2ray-agent.sh
@@ -3,7 +3,7 @@
 clear
 
 echo " ===================================== "
-echo "|      V2Ray-Agent Installation       |""
+echo "|      V2Ray-Agent Installation       |"
 echo "|       Autoscript By John Saka       |"
 echo " ===================================== "
 sleep 1
@@ -14,18 +14,33 @@ sleep 1
 echo "Installation start in 1 seconds"
 sleep 1
 
-echo "Remove unused packages"
+echo "Remove unused packages:"
 sleep 1
 apt autoremove -y && apt autoclean -y
+echo ""
 
-echo "Check for packages update"
+echo "Check for packages update:"
 sleep 1
 apt update
+echo ""
 
-echo "View all update available"
+echo "View all update available:"
 sleep 1
 apt list --upgradable
+echo ""
 
-echo "Update available packages"
+echo "Update available packages:"
 sleep 1
 apt upgrade -y
+echo ""
+
+echo "Install Speedtest CLI:"
+sleep 1
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt install speedtest
+echo ""
+
+echo "Install V2Ray-Agent:"
+sleep 1
+wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-agent/master/shell/install_en.sh" && chmod 700 /root/install_en.sh && mv /root/install_en.sh /root/install.sh && /root/install.sh
+1 -y && 2 -y
